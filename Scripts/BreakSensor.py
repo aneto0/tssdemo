b378e58efa01848127d55b77a4e3da572dacd35c
netoa <Andre.Neto@iter.org>
2020-01-12 15:02:19 +0000

    Change BreakSensor to whatever

--- Scripts/BreakSensor.py
+++ Scripts/BreakSensor.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 numberOfSensors = 24
 sectors = [1, 3, 4, 6, 7, 9]
      
-reducedSensorSize = 100 
+reducedSensorSize = 90
 dataFileName='55A0-bestip-config-demo-sensor-data.bin'
 def breakSensor(sector, sensor):
     pvname = "55A0-BESTIP-AA{0}-SENSORS-LAST".format(sector)
